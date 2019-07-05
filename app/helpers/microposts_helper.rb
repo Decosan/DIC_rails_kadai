ae42ca1d907b9f4802db1b8196fbd5d064d82cc4
Decosan <junx0401@gmail.com>
2019-07-05 09:06:29 +0900

    Helperを作成してNEWとEditのルーティングを分岐させた

--- app/helpers/microposts_helper.rb
+++ app/helpers/microposts_helper.rb
@@ -2,7 +2,7 @@ module MicropostsHelper
   def choose_new_or_edit
     if action_name=='new'||action_name=='confirm'
       confirm_microposts_path
-    else ation_name=='edit'
+    else action_name=='edit'
       micropost_path
     end
   end
